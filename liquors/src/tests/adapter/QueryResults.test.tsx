7f20be74a566c3fd8cac12aee3759267dac9f0db
winterhart <m.user.work@gmail.com>
2019-04-21 22:01:11 -0400

    Removing unecessary loop

--- liquors/src/tests/adapter/QueryResults.test.tsx
+++ liquors/src/tests/adapter/QueryResults.test.tsx
@@ -1,8 +1,5 @@
 import {QueryResultMapper} from '../../domain/adapters/QueryResultMapper';
 import { QueryResult } from '../../domain/model/QueryResult';
-import { Liquor } from '../../domain/model/Liquor';
-import { LiquorDetails } from '../../domain/model/LiquorDetails';
-import { LiquorProductor } from '../../domain/model/LiquorProductor';
 
 const rawJSON = require('../sample/sampleAPI.json');
 describe('Adapter Testing', () =>{
@@ -14,25 +11,14 @@ describe('Adapter Testing', () =>{
             (results.numberOfResult === 44 &&
                 results.queryResponseTime == 102 &&
                 results.Liquors.length ==10)
-        
-        for(let index = 0; index <  results.Liquors.length; index++){
+    
+            expect(results.Liquors[9].details.productor.country).toEqual('Canada');
+            expect(results.Liquors[9].details.size).toEqual('750 ml');
+            expect(results.Liquors[9].price).toEqual(39.75);
+            expect(results.Liquors[3].details.productor.name).toEqual('White Oak Distillery');
+            expect(results.Liquors[3].details.bouchonType).toEqual('Métal vissé');
+            expect(results.Liquors[3].imageURL).toEqual('http://s7d9.scene7.com/is/image/SAQ/13738354_is');
 
-            const liquor : Liquor = results.Liquors[index];
-            const liqDetails : LiquorDetails = liquor.details;
-            const liqProd : LiquorProductor = liqDetails.productor;
-
-            if(index === 9){
-                expect(liqProd.country).toEqual('Canada');
-                expect(liqDetails.size).toEqual('750 ml');
-                expect(liquor.price).toEqual(39.75);
-            }
-
-            if(index === 3){
-                expect(liqProd.name).toEqual('White Oak Distillery');
-                expect(liqDetails.bouchonType).toEqual('Métal vissé');
-                expect(liquor.imageURL).toEqual('http://s7d9.scene7.com/is/image/SAQ/13738354_is');
-            }
-        }
         expect(condition).toEqual(true);
     });
 })
